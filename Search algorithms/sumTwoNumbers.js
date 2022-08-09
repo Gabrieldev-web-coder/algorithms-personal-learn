fdf89ead4751bb3a63f248a3e3bfcb20c0d1b730
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-09 09:45:42 -0500

    adding more problems to solve

--- Search algorithms/sumTwoNumbers.js
+++ Search algorithms/sumTwoNumbers.js
@@ -1,29 +1,36 @@
-const arr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+import readline from "readline";
 
-const searchNumbers = (array, target) => {
-  let left = 0;
-  let rigth = array.length - 1;
-  let temp = 0;
-  let showcase;
-  while (left <= rigth) {
-    let mid = left + (rigth - left / 2);
-    showcase = mid;
-    if (array[mid] + array[temp] === target) {
-      return [mid, temp];
-    } else if (target < array[mid] + array[temp]) {
-      rigth = mid - 1;
-      temp = mid;
-    } else {
-      left = mid + 1;
-      temp = mid;
+const prompt = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+prompt.question("Insert a target for search combination between 1 and 20: ", (number) => {
+  const userInput = Number(number);
+  const arr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
+  const searchNumbers = (array, target) => {
+    let left = 0;
+    let rigth = array.length - 1;
+    let temp = 0;
+    while (left <= rigth) {
+      let mid = left + (rigth - left / 2);
+      if (array[mid] + array[temp] === target) {
+        return [mid, temp];
+      } else if (target < array[mid] + array[temp]) {
+        rigth = mid - 1;
+        temp = mid;
+      } else {
+        left = mid + 1;
+        temp = mid;
+      }
     }
-  }
-  let possible = (temp + showcase);
-  return "Not found combination " + array[possible] + " " + possible;
-};
+    return "Not found combination ";
+  };
 
-console.log(searchNumbers(arr, 18));
-// let i = arr[4] + arr[4] - 1;
-// console.log(arr[i]);
+  console.log(searchNumbers(arr, userInput));
+
+  prompt.close();
+});
---
+++ freecodecamp/chunkArrayInGroups.js
@@ -0,0 +1,4 @@
+function chunkArrayInGroups(arr, size) {
+  return arr;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
