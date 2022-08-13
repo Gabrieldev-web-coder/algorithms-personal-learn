1253176bb385bd0c4d9749dcdb3eb3c1323d5bb5
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-13 13:28:45 -0500

    freecodecamp problem #1 solved with brute force

--- freecodecamp/chunkArrayInGroups.js
+++ freecodecamp/chunkArrayInGroups.js
@@ -1,4 +1,19 @@
 function chunkArrayInGroups(arr, size) {
-  return arr;
+  //Brute force solution...
+  const subArrays = [];
+
+  while (arr.length > 0) {
+    // 0(n)
+    const subArr = arr.splice(0, size); // splice method is 0(n)
+    if (subArr.length !== 0) {
+      subArrays[subArrays.length] = subArr;
+    }
+  }
+  return subArrays;
 }
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+//Therefore, this algorithm is 0(n2)
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
+module.exports = chunkArrayInGroups;
