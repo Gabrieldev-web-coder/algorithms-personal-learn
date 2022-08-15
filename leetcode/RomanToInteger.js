799fdce6a2fda1e675bbe3530f3ffc8ddb2ea2d0
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-15 17:11:04 -0500

    resolving roman to integer problem from leet cod e(4h)

--- leetcode/RomanToInteger.js
+++ leetcode/RomanToInteger.js
@@ -16,17 +16,42 @@ function romanToInt(s) {
     // Lookup time constant by map.
     j++;
     if (romanNumerals.get(romanNums[i]) < romanNumerals.get(romanNums[j])) {
+      let front = romanNumerals.get(romanNums[i + 1]);
+      let back = romanNumerals.get(romanNums[i - 1]);
+      let mid = romanNumerals.get(romanNums[i]);
       let existentIndex = romanNums[j] ? romanNums[j] : romanNums[j - 1];
       let subSubstract =
-        romanNumerals.get(romanNums[i]) - romanNumerals.get(existentIndex);
-      count = count - subSubstract;
+        romanNumerals.get(existentIndex) - romanNumerals.get(romanNums[i]);
+
+      if (back && front > mid) {
+        let exponentPlus = back + front - mid;
+        console.log(exponentPlus);
+        count = exponentPlus;
+      } else {
+        count = count - subSubstract;
+      }
     } else if (
       romanNumerals.get(romanNums[i]) > romanNumerals.get(romanNums[j])
     ) {
-      let existentIndex = romanNums[j] ? romanNums[j] : romanNums[j - 1];
-      let subPlus =
-        romanNumerals.get(romanNums[i]) + romanNumerals.get(existentIndex);
-      count = count + subPlus;
+      let front = romanNumerals.get(romanNums[i + 1]);
+      let back = romanNumerals.get(romanNums[i - 1]);
+      let mid = romanNumerals.get(romanNums[i]);
+
+      if (
+        romanNumerals.get(romanNums[j]) === romanNumerals.get(romanNums[j + 1])
+      )
+        continue;
+
+      if (back && front > mid) {
+        let exponentPlus = back + front - mid;
+        console.log(exponentPlus);
+        count = count = +exponentPlus;
+      } else {
+        let existentIndex = romanNums[j] ? romanNums[j] : romanNums[j - 1];
+        let subPlus =
+          romanNumerals.get(romanNums[i]) + romanNumerals.get(existentIndex);
+        count = count + subPlus;
+      }
     } else {
       count = count + romanNumerals.get(romanNums[i]);
     }
@@ -34,6 +59,7 @@ function romanToInt(s) {
   return count;
 }
 
-console.log(romanToInt("V"));
+//console.log(romanToInt("XIX"));
+//console.log(romanToInt("XV"));
 
 module.exports = romanToInt;
