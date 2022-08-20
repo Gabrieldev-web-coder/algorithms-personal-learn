b5b2e95d8bcb3ff188e2ea3ef761d7b3aee5eb4c
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-19 20:11:08 -0500

    roman to integer solution/leetcode with mapping and for loop. 19/08/2022

--- leetcode/RomanToInteger.js
+++ leetcode/RomanToInteger.js
@@ -1,5 +1,5 @@
 function romanToInt(s) {
-  const romanNumerals = new Map([
+  const map = new Map([
     ["I", 1],
     ["V", 5],
     ["X", 10],
@@ -8,58 +8,40 @@ function romanToInt(s) {
     ["D", 500],
     ["M", 1000],
   ]);
-  const romanNums = s.split(""); //O(n)
   let count = 0;
-  let j = 0;
-
-  for (let i = 0; i < romanNums.length; i++) {
-    // Lookup time constant by map.
-    j++;
-    if (romanNumerals.get(romanNums[i]) < romanNumerals.get(romanNums[j])) {
-      let front = romanNumerals.get(romanNums[i + 1]);
-      let back = romanNumerals.get(romanNums[i - 1]);
-      let mid = romanNumerals.get(romanNums[i]);
-      let existentIndex = romanNums[j] ? romanNums[j] : romanNums[j - 1];
-      let subSubstract =
-        romanNumerals.get(existentIndex) - romanNumerals.get(romanNums[i]);
-
-      if (back && front > mid) {
-        let exponentPlus = back + front - mid;
-        console.log(exponentPlus);
-        count = exponentPlus;
-      } else {
-        count = count - subSubstract;
-      }
-    } else if (
-      romanNumerals.get(romanNums[i]) > romanNumerals.get(romanNums[j])
-    ) {
-      let front = romanNumerals.get(romanNums[i + 1]);
-      let back = romanNumerals.get(romanNums[i - 1]);
-      let mid = romanNumerals.get(romanNums[i]);
-
-      if (
-        romanNumerals.get(romanNums[j]) === romanNumerals.get(romanNums[j + 1])
-      )
-        continue;
-
-      if (back && front > mid) {
-        let exponentPlus = back + front - mid;
-        console.log(exponentPlus);
-        count = count = +exponentPlus;
-      } else {
-        let existentIndex = romanNums[j] ? romanNums[j] : romanNums[j - 1];
-        let subPlus =
-          romanNumerals.get(romanNums[i]) + romanNumerals.get(existentIndex);
-        count = count + subPlus;
-      }
+  for (let i = 0, j = i + 1; i < s.length; i++, j++) {
+    if (map.get(s[i]) === "I" && map.get(s[j]) === "V") {
+      count += 4;
+      i++;
+    } else if (map.get(s[i]) === "I" && map.get(s[j]) === "X") {
+      count += 9;
+      i++;
+    } else if (map.get(s[i]) === "X" && map.get(s[j]) === "L") {
+      count += 40;
+      i++;
+    } else if (map.get(s[i]) === "X" && map.get(s[j]) === "C") {
+      count += 90;
+      i++;
+    } else if (map.get(s[i]) === "C" && map.get(s[j]) === "D") {
+      count += 400;
+      i++;
+    } else if (map.get(s[i]) === "C" && map.get(s[j]) === "M") {
+      count += 900;
+      i++;
     } else {
-      count = count + romanNumerals.get(romanNums[i]);
+      count += map.get(s[i]);
     }
   }
+
   return count;
 }
 
-//console.log(romanToInt("XIX"));
-//console.log(romanToInt("XV"));
+/* 
+
+Actually learned.
+
+I forgot that strings are iterable, mapping was aplied successfuly, we can define rules to solve a problems
+
+*/
 
 module.exports = romanToInt;
