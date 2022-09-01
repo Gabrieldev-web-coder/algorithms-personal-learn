f7f3ee13962e9fa0451ef4769867c77cf81cd832
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-31 22:17:39 -0500

    horizontal approach longest common prefix

--- leetcode/LongestCommonPrefix.js
+++ leetcode/LongestCommonPrefix.js
@@ -1,3 +1,12 @@
-function longestCommonPrefix(strs) {}
+function longestCommonPrefix(strs) {
+  let prefix = strs[0];
+  for (let i = 0; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) != 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix.length === 0) return "";
+    }
+  }
+  return prefix;
+}
 
-console.log(longestCommonPrefix(["flower", "flower", "flower", "flower"]));
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
